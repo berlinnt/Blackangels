85ae61ac9e5fdc7b6f5686c1f1bf5fa78f847bc4
berlinnt <lancecrismas112233@gmail.com>
2024-05-27 05:40:04 +0700

    FIKS tagall.py

--- MukeshRobot/modules/tagall.py
+++ MukeshRobot/modules/tagall.py
@@ -58,10 +58,10 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{random.choice(emoji_tag)}](tg://user?id={usr.id})"
+        usrtxt += f"❍ [{usr.first_name}](tg://user?id={usr.id})\n"
         if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{usrtxt}\n\n{msg}"
+                txt = f"{msg}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
